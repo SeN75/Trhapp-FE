33a63f7ecec9598507554973cec2d46e2ba27d58
SeN75 <saa111954@hotmail.com>
2024-05-28 20:45:50 +0300

    :wrench: added a guard for layout

--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, inject } from '@angular/core';
-import { BehaviorSubject, catchError, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, tap } from 'rxjs';
 import { Auth } from '../types/base.type';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { LoggerService } from './logger.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private auth = new BehaviorSubject<Auth>({} as Auth);
+  private router = inject(Router);
+  private cookie = inject(CookieService);
   get auth$() {
     return this.auth.asObservable();
   }
@@ -21,7 +25,7 @@ export class AuthService {
     this.getAuth();
   }
   getAuth() {
-    const data = localStorage['token'] as string;
+    const data = this.cookie.get('TP-Auth');
     return data ? (JSON.parse(data) as Auth) : null;
   }
   login({
@@ -35,7 +39,7 @@ export class AuthService {
       .post<Auth>(`${this.URL}login/`, { phone_number, password })
       .pipe(
         tap(({ access, refresh }) => {
-          localStorage.setItem('token', JSON.stringify({ access, refresh }));
+          this.setToken = { access, refresh };
           this.auth.next({ access, refresh });
         }),
         catchError((error) => {
@@ -67,6 +71,40 @@ export class AuthService {
         catchError((error) => {
           this.logger.error(error);
           return error;
+        })
+      );
+  }
+  set setToken(token: { access: string; refresh: string }) {
+    this.cookie.set('TP-Auth', JSON.stringify(token), {
+      expires: new Date(new Date().setDate(new Date().getDate() + 1)),
+    });
+  }
+  get token() {
+    const _token = JSON.parse(this.cookie.get('TP-Auth') || '{}') as Auth;
+    return _token;
+  }
+
+  logout() {
+    this.cookie.delete('TP-Auth');
+    this.auth.next({} as Auth);
+    this.router.navigate(['/', 'r', 'login']);
+  }
+  refreshToken(): Observable<Auth> {
+    return this.http
+      .post<Auth>(`${this.URL}refresh/`, {
+        refresh: this.token.refresh,
+      })
+      .pipe(
+        map(({ access, refresh }) => {
+          return { access, refresh } as Auth;
+        }),
+        tap(({ access, refresh }) => {
+          this.setToken = { access, refresh };
+          this.auth.next({ access, refresh });
+        }),
+        catchError((error) => {
+          this.logger.error(error);
+          throw error;
         })
       );
   }
