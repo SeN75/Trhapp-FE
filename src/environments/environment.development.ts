529be82d4ab17a56e56c2d93549740f88af4d400
Ahlam Saleh <Ahlam.saleh321@gmail.com>
2024-06-04 23:28:29 +0300

    updated environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/',
-  authUrl: 'http://127.0.0.1:8000/auth/',
-  opsUrl: 'http://127.0.0.1:8000/ops/',
-  pilgrimUrl: 'http://127.0.0.1:8000/pilgrim/',
+  apiUrl: 'https://api-alelyani.trhapp.net/api/',
+  authUrl: 'https://api-alelyani.trhapp.net/auth/',
+  opsUrl: 'https://api-alelyani.trhapp.net/ops/',
+  pilgrimUrl: 'https://api-alelyani.trhapp.net/pilgrim/',
   registration_code: 'tmY0k2AD6CcGtrhappQMawLmq',
 };
